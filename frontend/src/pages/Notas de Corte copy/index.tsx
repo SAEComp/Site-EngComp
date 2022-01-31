1b391ff27f89c3583b42dd8d884bbad49810e841
Rafael Takano <49351132+Rafael-Takano@users.noreply.github.com>
2022-01-31 18:03:43 -0300

    Adicão de pastas para as páginas estáticas

---
+++ frontend/src/pages/Notas de Corte copy/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {Container, Wrapper} from './styles';
+
+import NavBar from "../../components/NavBar";
+import Header from "../../components/Header";
+import WhatIs from "../../components/WhatIs";
+import Extras from "../../components/Extra";
+import Footer from "../../components/Footer";
+
+const Home:React.FC = ()=>{
+    return(
+       <Container>
+          <NavBar />
+          <Wrapper>
+            <Header 
+            title="ENGENHARIA DE COMPUTAÇÃO"
+            text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel nam recusandae velit sapiente perferendis repudiandae eum provident aut! Nobis quas magnam reprehenderit accusantium dolorum quia eos necessitatibus fugit non fuga!
+            "
+            />
+            <WhatIs />
+            <Extras 
+            title = "EXTRACURRICULARES" 
+            text = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel nam recusandae velit sapiente perferendis repudiandae eum provident aut! Nobis quas magnam reprehenderit accusantium dolorum quia eos necessitatibus fugit non fuga!"
+            />
+            <Footer />
+          </Wrapper>
+       </Container>
+    )
+}
+
+export default Home;
