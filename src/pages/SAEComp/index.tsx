db7835577603d29ce4050a20cb697dd3493c83ab
JoaoMFB <114200997+JoaoMFB@users.noreply.github.com>
2022-11-29 20:10:23 -0300

    teste

--- src/pages/SAEComp/index.tsx
+++ src/pages/SAEComp/index.tsx
@@ -8,23 +8,7 @@ import Footer from "../../components/Footer";
 
 const SAEcomp = ()=>{
     return(
-       <Container>
-          <NavBar/>
-          <Wrapper>
-            <Header2
-            title="CONHEÇA A SECRETARIA ACADÊMICA"
-            text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel nam recusandae velit sapiente perferendis repudiandae eum provident aut! Nobis quas magnam reprehenderit accusantium dolorum quia eos necessitatibus fugit non fuga!
-            "
-            url = "../../assets/img/background1.jpg"
-            />
-            <ProdutosSaecomp/>
-            <Extras 
-            title = "EXTRACURRICULARES" 
-            text = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel nam recusandae velit sapiente perferendis repudiandae eum provident aut! Nobis quas magnam reprehenderit accusantium dolorum quia eos necessitatibus fugit non fuga!"
-            />
-            <Footer />
-          </Wrapper>
-       </Container>
+      <h1>Hello world</h1>
     )
 }
 
