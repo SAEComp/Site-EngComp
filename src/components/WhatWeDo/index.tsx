82e03fdddef142a83b8e3f860769c175e4c306aa
JoaoMFB <114200997+JoaoMFB@users.noreply.github.com>
2023-02-24 01:04:20 -0300

    whatwedo fix

--- src/components/WhatWeDo/index.tsx
+++ src/components/WhatWeDo/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { WhatWeDoDiv, Icons, Text1, Website, Paragraph, Paragraph1, Paragraph2, Paragraph3, Paragraph5, FlexColumn, FlexColumn1,
-     FlexColumn2, Block, Block1, Block2, Block3, Block4, Block5 } from "./styles";
+import {WhatwedoDiv,WhatwedoDiv2,WhatwedoDiv3, Container  } from "./styles";
+import CardWhatwedo from '../CardWhatwedo'
 import img1 from '../../assets/img/brain.png';
 import img2 from '../../assets/img/blusa.png';
 import img3 from '../../assets/img/prancheta.png';
@@ -11,65 +11,20 @@ import img6 from '../../assets/img/festa.png';
 
 const WhatWeDo:React.FC = ()=> {
   return (
-    <WhatWeDoDiv>
-      <FlexColumn>
-        <Text1>O QUE FAZEMOS</Text1>
-        <Block>
-          <Icons
-            src={img1}
-          />
-          <Paragraph>
-            Organizamos diversos minicursos para dentro e fora da
-            {"  "}
-            USP
-          </Paragraph>
-        </Block>
-        <Block3>
-          <Icons
-            src={img4}
-          />
-          <Paragraph1>
-            Coletamos feedback sobre os professores para buscar sempre melhorar
-            o curso
-          </Paragraph1>
-        </Block3>
-      </FlexColumn>
-      <FlexColumn1>
-        <Block1>
-          <Icons
-            src={img6}
-          />
-          <Paragraph2>
-            Cuidamos da recepção e integração dos bixos todos os anos
-          </Paragraph2>
-        </Block1>
-        <Block4>
-          <Icons
-            src={img3}
-          />
-          <Paragraph3>
-            Planejamos e vendemos os produtos da Engenharia de Computação
-            <br />
-          </Paragraph3>
-        </Block4>
-      </FlexColumn1>
-      <FlexColumn2>
-        <Block2>
-          <Icons
-            src={img2}
-          />
-          <Paragraph2>
-            Gamenights, Sessão de Filmes, Festas e muitos outros eventos
-          </Paragraph2>
-        </Block2>
-        <Block5>
-          <Website src={img5} />
-          <Paragraph5>
-            Fazemos parte do grupo que criou o site da Engenharia de Computação
-          </Paragraph5>
-        </Block5>
-      </FlexColumn2>
-    </WhatWeDoDiv>
+      <Container>
+        <WhatwedoDiv>
+          <CardWhatwedo url = "img1" text = "Organizamos diversos minicursos para dentro e fora da USP"></CardWhatwedo>
+          <CardWhatwedo url = "img2" text = "Cuidamos da recepção e integração dos bixos todos os anos"></CardWhatwedo>
+        </WhatwedoDiv>
+        <WhatwedoDiv2>
+          <CardWhatwedo url = "img3" text = "Gamenights, sessões de filmes, festas e muitos outros eventos"></CardWhatwedo>
+          <CardWhatwedo url = "img4" text = "Coletamos feedbacks sobre os professores para podermos melhorar o curso"></CardWhatwedo>
+        </WhatwedoDiv2>
+        <WhatwedoDiv3>
+          <CardWhatwedo url = "img5" text = "Planejamos e vendemos os produtos da Engenharia de Computação"></CardWhatwedo>
+          <CardWhatwedo url = "img6" text = "Fazemos parte do grupo que criou o site da Engenharia de Computação"></CardWhatwedo>
+        </WhatwedoDiv3>
+      </Container>
   );
 }
 
--- src/components/WhatWeDo/styles.tsx
+++ src/components/WhatWeDo/styles.tsx
@@ -1,161 +1,32 @@
 import styled from 'styled-components';
 
-
-
-export const Icons = styled.img`
-  width: 240px;
-  height: 240px;
-  align-self: stretch;
-`;
-export const Paragraph2 = styled.div`
-  width: 228px;
-  height: 75px;
-  color: #828282;
-  font-size: 16px;
-  font-weight: 700;
-  font-family: Oxygen;
-  text-align: center;
-`;
-export const WhatWeDoDiv = styled.div`
+export const Container = styled.div`
   margin: 10rem;
-  width: 1119px;
+  padding-bottom: 5rem;
   display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-`;
-export const FlexColumn = styled.div`
-  width: 306px;
-  height: 900px;
-  display: flex;
+  justify-content: space-between;
+  gap: 3vw;
+  @media(max-width: 1080px){
+    margin: 0 auto;
+  }
+`
+export const WhatwedoDiv = styled.div`
+  display:flex;
   flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-end;
-  margin: 0px 157px 0px 0px;
-`;
-export const Text1 = styled.div`
-  align-self: flex-start;
+  justify-content: space-around;
+  gap: 2vw;
+`
+export const WhatwedoDiv2 = styled.div`
+  display:flex;
+  flex-direction: column;
+  justify-content: space-around;
+  gap: 2vw;
+`
+export const WhatwedoDiv3 = styled.div`
+  display:flex;
+  flex-direction: column;
+  justify-content: space-around;
+  gap: 2vw;
+`
 
-  color: #1a6930;
-  font-size: 3rem;
-  font-weight: 700;
-  font-family: Ubuntu;
-  white-space: nowrap;
-`;
-export const Block = styled.div`
-  width: 240px;
-  gap: 25px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-end;
-  margin: 0px 1px 92px 0px;
-`;
-export const Paragraph = styled.div`
-  width: 228px;
-  height: 60px;
-  margin: 0px 5px 0px 0px;
-  color: #828282;
-  font-size: 16px;
-  font-weight: 700;
-  font-family: Oxygen;
-  text-align: center;
-  white-space: pre-wrap;
-`;
-export const Block3 = styled.div`
-  width: 240px;
-  gap: 13px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-`;
-export const Paragraph1 = styled.div`
-  width: 228px;
-  height: 75px;
-  margin: 0px 0px 0px 5px;
-  color: #828282;
-  font-size: 16px;
-  font-weight: 700;
-  font-family: Oxygen;
-  text-align: center;
-`;
-export const FlexColumn1 = styled.div`
-  width: 244px;
-  height: 818px;
-  gap: 77px;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: flex-start;
-  margin: 0px 140px 0px 0px;
-  padding: 41px 0px;
-`;
-export const Block1 = styled.div`
-  width: 240px;
-  gap: 12px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-self: flex-end;
-  align-items: center;
-`;
-export const Block4 = styled.div`
-  width: 240px;
-  gap: 25px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-end;
-`;
-export const Paragraph3 = styled.div`
-  width: 228px;
-  height: 75px;
-  margin: 0px 4px 0px 0px;
-  color: #828282;
-  font-size: 16px;
-  font-weight: 700;
-  font-family: Oxygen;
-  text-align: center;
-`;
-export const FlexColumn2 = styled.div`
-  width: 272px;
-  height: 900px;
-  gap: 120px;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: flex-end;
-`;
-export const Block2 = styled.div`
-  width: 240px;
-  gap: 10px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: 0px 14px 0px 0px;
-`;
-export const Block5 = styled.div`
-  width: 272px;
-  gap: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-self: stretch;
-  align-items: center;
-`;
-export const Website = styled.img`
-  width: 240px;
-  height: 240px;
-`;
-export const Paragraph5 = styled.div`
-  width: 272px;
-  height: 114px;
-  align-self: stretch;
-  color: #828282;
-  font-size: 16px;
-  font-weight: 700;
-  font-family: Oxygen;
-  text-align: center;
-`;
+
