5b662d6e475e87f24afb74f7d6140b2616c22ab4
Joao Augusto <67336838+joaoaugusto05@users.noreply.github.com>
2023-05-27 18:22:53 -0300

    Adptading css

--- src/pages/Login/styles.tsx
+++ src/pages/Login/styles.tsx
@@ -11,11 +11,13 @@ export const LoginCard = styled.div`
     display: flex;
     justify-items: center;
     align-items: center;
+    width: 15vw;
+    height: 25vh;
     gap:10px;
     flex-direction: column;
     border: 1px solid gray;
     border-radius: 10%;
-    background-color: rgb(205, 205, 203);
+    background-color: rgb(205, 205, 203, 0.3);
     box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
 
 `
