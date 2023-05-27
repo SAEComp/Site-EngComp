321b1c6565549c3807f66bb6591ee14fb27219c8
Joao Augusto <67336838+joaoaugusto05@users.noreply.github.com>
2023-05-27 18:18:47 -0300

    box-shadow and radius test

--- src/pages/Login/styles.tsx
+++ src/pages/Login/styles.tsx
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    display: flex;
     padding: 15rem;
+    align-items: center;
+    justify-content: center;
 `
 
 export const LoginCard = styled.div`
@@ -9,4 +12,10 @@ export const LoginCard = styled.div`
     justify-items: center;
     align-items: center;
     gap:10px;
+    flex-direction: column;
+    border: 1px solid gray;
+    border-radius: 10%;
+    background-color: rgb(205, 205, 203);
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+
 `
