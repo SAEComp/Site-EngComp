5265629242665bfdcfb9e5b974c75ef025e3496b
JoaoMFB <jfbattaglini@gmail.com>
2023-06-23 11:54:33 -0300

    .env

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import _Routes from './routes' ;
-import { initializeApp } from 'firebase/app';
-import { config } from '../src/config/config';
-import { getFirestore } from 'firebase/firestore';
-
-export const Firebase = initializeApp(config.firebaseConfig);
 
 function App() {
   return <_Routes />;
