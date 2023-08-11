f2fd75ef546ff978cfaa19b3f2c709ff4ca1ae96
JoaoMFB <jfbattaglini@gmail.com>
2023-08-11 17:40:19 -0300

    Update firebase.ts

--- src/auth/firebase.ts
+++ src/auth/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-        apiKey: process.env.API_KEY,
-        authDomain: process.env.AUTH_DOMAIN,
-        projectId: process.env.PROJECT_ID,
-        storageBucket: process.env.STORAGE_BUCKET,
-        messagingSenderId: process.env.MESSAGING_SENDER_ID,
-        appId: process.env.APP_ID,
-        measurementId: process.env.MEASUREMENT_ID,
+    apiKey: "AIzaSyAixkzovn8wfhlbY1V0vl1XK05kBwD0Pzc",
+    authDomain: "deploy-firebase-22ec1.firebaseapp.com",
+    projectId: "deploy-firebase-22ec1",
+    storageBucket: "deploy-firebase-22ec1.appspot.com",
+    messagingSenderId: "608281611540",
+    appId: "1:608281611540:web:8f9e51c3492b532ba90601",
+    measurementId: "G-EBLZ22P3BP"
     }
 
 const app = initializeApp(firebaseConfig);
