de8ff39e69d603c77977db090a93825699fc47fd
JeanCassiano <115122121+JeanCassiano@users.noreply.github.com>
2024-01-29 13:37:49 -0300

    bug fix

--- src/components/WhatEnf/index.tsx
+++ src/components/WhatEnf/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { WhatEnfDiv, Grid, Container, Enf_img } from "./styles";
 import Enfases_img from "../../assets/img/Enfases.png"
-import {Defaultmargin, DefaultTitle } from "../../pages/Default/styles";
+import {Defaultmargin, DefaultTitle, StandardText } from "../../pages/Default/styles";
 
 
 const WhatEnf:React.FC = ()=>{
@@ -10,14 +10,25 @@ const WhatEnf:React.FC = ()=>{
         <Container>
         <Defaultmargin>
         <WhatEnfDiv>
-            <DefaultTitle style = {{color: '#003CC5'}}>TUDO SOBRE ÊNFASES</DefaultTitle>
             <Grid>
                 <div className="explanation">
+                <DefaultTitle style = {{color: '#003CC5'}}>O QUE SÃO ÊNFASES?</DefaultTitle>
+                <div style = {{color:'#545454', display:'grid', gridGap:'1.25rem', marginBottom: '1.2vw'}}>
                     <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Accumsan in nisl nisi scelerisque eu. A erat nam at lectus urna duis. Nunc sed id semper risus. Faucibus vitae aliquet nec ullamcorper. Felis donec et odio pellentesque diam volutpat commodo. Eget mi proin sed libero. Vitae congue eu consequat ac felis donec et odio. Amet dictum sit amet justo donec enim diam vulputate ut. Dui ut ornare lectus sit amet est placerat. Convallis posuere morbi leo urna molestie at elementum eu. Ullamcorper a lacus vestibulum sed arcu non odio. Adipiscing elit duis tristique sollicitudin nibh sit. Mauris a diam maecenas sed enim ut. Tellus id interdum velit laoreet id. Duis ut diam quam nulla porttitor massa. Eget mauris pharetra et ultrices neque. Sit amet dictum sit amet justo donec enim diam. Non sodales neque sodales ut etiam sit.
+                    <StandardText>
+                    As ênfases são focos de estudo dentro de um curso universitário, permitindo que os estudantes explorem a fundo determinadas áreas temáticas relacionadas à sua formação acadêmica. Essa abordagem oferece uma experiência mais personalizada e alinhada aos objetivos de carreira de cada aluno, permitindo-lhes aprofundar seus conhecimentos em áreas específicas de seu campo de estudo.
+
+                    Além disso, as ênfases podem agregar valor ao currículo dos estudantes, destacando suas habilidades e conhecimentos especializados ao procurar oportunidades de estágio, emprego ou pós-graduação. Empresas e instituições acadêmicas muitas vezes valorizam candidatos que demonstram um compromisso sólido e uma expertise particular em um determinado domínio.
+                    </StandardText>
                     </p>
-                    <p>Quis enim lobortis scelerisque fermentum dui faucibus. Eget mauris pharetra et ultrices neque ornare. Non pulvinar neque laoreet suspendisse. Quis eleifend quam adipiscing vitae proin. Mauris in aliquam sem fringilla ut morbi tincidunt augue interdum. Euismod nisi porta lorem mollis aliquam ut. Velit dignissim sodales ut eu sem integer vitae. Ut placerat orci nulla pellentesque dignissim enim sit amet. Sit amet porttitor eget dolor morbi non. Purus sit amet volutpat consequat mauris nunc congue nisi vitae. Faucibus purus in massa tempor nec.                    
+                    <p>
+                    <StandardText>
+
+                    Cada ênfase é uma oportunidade de aprofundar-se em áreas específicas. Ao escolher uma ênfase, os estudantes abrem portas para uma compreensão mais refinada e especializada, permitindo que moldem sua formação de acordo com suas aspirações profissionais.
+                    
+                    </StandardText>
                     </p>
+                </div>
                 </div>
             </Grid> 
         </WhatEnfDiv>
